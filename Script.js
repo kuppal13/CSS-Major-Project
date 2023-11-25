9d56f0d45eb43de40aa688b002771bf648486c9e
kuppal13 <148819621+kuppal13@users.noreply.github.com>
2023-11-24 19:44:34 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+
+    function changeColor() {
+      document.body.style.backgroundColor = "purple";
+
+    }
+    
+    function changeFontColor() {
+        document.getElementById("change").style.color = "#29e2e2";
+      
+        
+    }   
+  
